22d6e52838f8724a4c554711eb9b364c8ddf9f7d
flashgrey <aaftabkhan2005@gmail.com>
2025-07-25 16:36:21 +0530

    test changes

--- test.py
+++ test.py
@@ -7,16 +7,16 @@ from utils import display_on_linux
 from core import invert,threshold, threshold_binary,correlate, convolution
 
 # img = cv2.imread("assets/lucy_moon.jpg")
-img = cv2.imread("assets/lucy_moon.jpg")
-
+img = cv2.imread("assets/anne_listenin.jpg")
 # img = invert(img)
-# img = threshold(img, 0)
+# img = threshold(img, 128)
 # img = threshold_binary(img, 128)
 # print(len(img[0][0]))
-# convolution(img, np.array([[-1,-1,-1],[-1,8,4],[-1,-1,4]]))
+img = convolution(img, np.array([[-1,-1,-1],[-1,8,-1],[-1,-1,-1]]))
 # img = correlate(img, np.array([[-1,-1,-1],[-1,8,-1],[-1,-1,-1]]))
 # cv2.imwrite('assets/new.jpg', img)
 # display_on_linux("assets/new.jpg")
 # img3 = threshold(img, 198)
-# cv2.imwrite('assets/new2.jpg', img3)
-display_on_linux("assets/new.jpg")
+cv2.imwrite('assets/anne_listenin_conv.jpg', img)
+# display_on_linux("assets/new.jpg")
+display_on_linux("assets/anne_listenin_conv.jpg")
